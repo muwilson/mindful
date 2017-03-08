51e94564992cc324fa71e375596460b538f0c391
Tahmid Hasan <Tahmid Hasan>
2017-03-08 14:19:02 -0500

    added mock functionality for visualization

--- app/src/main/java/com/example/myapplication/myapplication/ViewingActivity.java
+++ app/src/main/java/com/example/myapplication/myapplication/ViewingActivity.java
@@ -2,12 +2,52 @@ package com.example.myapplication.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
 
-public class ViewingActivity extends AppCompatActivity {
+public class ViewingActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_viewing);
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.spinner1Items, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+
+        spinner = (Spinner) findViewById(R.id.spinner2);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        adapter = ArrayAdapter.createFromResource(this,
+                R.array.spinner2Items, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+
     }
+
+    public void onItemSelected(AdapterView<?> parent, View view,
+                               int pos, long id) {
+        ((TextView) findViewById(R.id.textView6)).setText("On a Good day");
+        ((TextView) findViewById(R.id.textView8)).setText("40% Not Stressed");
+        ((TextView) findViewById(R.id.textView7)).setText("40% Somewhat Stressed");
+        ((TextView) findViewById(R.id.textView9)).setText("20% Very Stressed");
+
+    }
+
+    public void onNothingSelected(AdapterView<?> parent) {
+        return;
+    }
+
 }
